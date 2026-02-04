83a2dfd6d46c5e4b4e5a0df87156c37e95281546
fpunny <frederic.pun@gmail.com>
2026-02-04 04:16:24 -0500

    task: fixed tailwind for landing

---
+++ packages/ui/src/index.d.ts
@@ -0,0 +1,8 @@
+import { ComponentPropsWithRef, ElementType } from 'react';
+
+export type Merge<A, B> = Omit<A, keyof B> & B;
+
+export type PolymorphicProps<Props, T extends ElementType> = Merge<
+  ComponentPropsWithRef<T>,
+  Merge<Props, { as?: T }>
+>;
