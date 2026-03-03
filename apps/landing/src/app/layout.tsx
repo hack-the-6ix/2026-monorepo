98427311a74a87971aaf0383b95b49517823db66
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-03-03 11:54:42 -0500

    fix: teeny tiny ui fixes (#4)
    * fix: input box border follows submit status and favicon
    
    * fix: changing location

--- apps/landing/src/app/layout.tsx
+++ apps/landing/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { assets } from '../lib/assets';
+
 import './globals.css';
 
 const inter = Inter({
@@ -13,8 +15,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: 'Hack the 6ix 2026',
-  description:
-    'Hack the 6ix 2026 - July 17-19, 2026 at Bahen Centre. In-Person.',
+  description: 'Hack the 6ix 2026 - July 17-19, 2026 in Toronto. In-Person.',
 };
 
 export default function RootLayout({
@@ -25,9 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+        <link rel="icon" href={assets.logo.src} type="image/svg+xml"></link>
       </head>
-      <body className={`${inter.className} ${inter.variable} antialiased h-full`} suppressHydrationWarning>
+      <body
+        className={`${inter.className} ${inter.variable} antialiased h-full`}
+        suppressHydrationWarning
+      >
         {children}
       </body>
     </html>
