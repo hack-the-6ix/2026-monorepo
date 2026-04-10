31e78552590eaf5c759a89f5d38cf3f70ea2e9a7
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-04-10 18:41:30 -0400

    desktop set up

--- apps/apply/src/app/page.tsx
+++ apps/apply/src/app/page.tsx
@@ -1,73 +1,69 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+import StepOne from '../app/_fake_page';
+import Couch from '../assets/couch.svg';
+import DrinkPortrait from '../assets/drink_portrait.svg';
+import Floor from '../assets/floor.png';
+import GlowSlant from '../assets/glow_slant.png';
+import Lamp from '../assets/lamp.svg';
+import NeedleThread from '../assets/needle_thread.svg';
+import Wall from '../assets/wall.png';
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+export default function Home() {
+  // TODO: mobile background, all the pages empty version
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({});
+
+  const nextStep = () => setStep((s) => s + 1);
+  const prevStep = () => setStep((s) => s - 1);
+
+  return (
+    <div className="font-sans min-h-screen p-8">
+      <div className="hidden md:block fixed inset-0 z-0 pointer-events-none bg-[linear-gradient(226deg,#F6BD55_-36.92%,#A53368_4.83%,#3E33AE_84.03%,#100C3F_149.65%)]">
+        <Image
+          src={Wall}
+          alt="Wall"
+          className="absolute top-0 w-full h-[95%] object-fill"
+        />
+        <Image
+          src={Floor}
+          alt="Floor"
+          className="absolute bottom-0 w-full h-[40%] object-fill"
+        />
+        <Image
+          src={DrinkPortrait}
+          alt="Drink Portrait"
+          className="absolute top-[5vw] lg:top-[4vw] -right-[3vw] lg:h-[8vw] w-auto h-[10vw]"
+        />
+        <Image
+          src={Couch}
+          alt="couch"
+          className="absolute bottom-0 -right-[5vw] w-auto h-[38vw]"
+        />
+        <Image
+          src={Lamp}
+          alt="Lamp"
+          className="absolute bottom-[1vw] -left-[5vw] w-auto h-[32vw]"
+        />
+        <Image
+          src={NeedleThread}
+          alt="Needle and threads"
+          className="absolute -bottom-[10vw] -right-[3vw] w-auto h-[28vw]"
+        />
+        <Image
+          src={GlowSlant}
+          alt="Glow"
+          className="absolute top-0 right-0 -translate-y-[5vw] translate-x-[5vw] w-full h-180 object-fill mask-[linear-gradient(to_bottom_left,black,transparent)]"
+        />
+        <div className="absolute inset-0 bg-[rgba(20,4,48,0.2)]"></div>
+      </div>
+
+      <main className="relative z-10 w-full max-w-md">
+        {step === 1 && <StepOne />}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
