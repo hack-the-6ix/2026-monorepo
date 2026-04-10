8e6318099ab285ee387fd4e05ac137e2d49ce4be
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-04-10 00:17:07 -0400

    clip path popup and hamburger transition

--- apps/landing/src/app/nav/NavLinks.tsx
+++ apps/landing/src/app/nav/NavLinks.tsx
@@ -12,13 +12,18 @@ const sectionLinks = [
   },
 ];
 
-export default function NavLinks() {
+interface NavLinksProps {
+  closeBurger?: () => void;
+}
+
+export default function NavLinks({ closeBurger }: NavLinksProps) {
   return (
     <>
       {sectionLinks.map((link, index) => (
         <Link
           key={`navlink-${index}`}
           href={link.href}
+          onClick={closeBurger}
           className="text-lg md:text-sm font-medium text-white hover:text-primary-200 transition-colors flex items-center gap-1.5 animate-text-glow"
         >
           {link.name}
--- apps/landing/src/app/nav/page.tsx
+++ apps/landing/src/app/nav/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { IoIosClose } from 'react-icons/io';
-import { RxHamburgerMenu } from 'react-icons/rx';
 import Image from 'next/image';
 import Link from 'next/link';
 
 import logo from '../../assets/Logo.svg';
+import legs from '../../assets/nav_legs.png';
 import body from '../../assets/nav_radish_body.png';
 import swing from '../../assets/nav_swing.png';
-import legs from '../../assets/nav_legs.png';
-import SocialLinks from './SocialLinks';
 import NavLinks from './NavLinks';
+import SocialLinks from './SocialLinks';
+
+import './index.css';
 
 export default function Nav() {
   const [visible, setVisible] = useState(true);
@@ -49,7 +49,7 @@ export default function Nav() {
     setTimeout(() => {
       setBurgerVisible(false);
       setIsClosing(false);
-    }, 1000);
+    }, 500);
   };
 
   return (
@@ -61,7 +61,11 @@ export default function Nav() {
       >
         <nav>
           <div className="mx-auto h-20 flex items-center gap-12">
-            <Link href="#hero" className="text-xl font-bold tracking-tighter">
+            <Link
+              href="#hero"
+              onClick={closeBurger}
+              className="text-xl font-bold tracking-tighter"
+            >
               <Image src={logo.src} alt="ht6 icon" width={20} height={20} />
             </Link>
             <div className="hidden md:flex gap-10">
@@ -74,9 +78,18 @@ export default function Nav() {
           <SocialLinks />
         </div>
         {/* Mobile hamburger */}
-        <div className="flex md:hidden h-20 items-center">
-          <button onClick={openBurger} className="text-white">
-            <RxHamburgerMenu size={28} />
+        <div className="md:hidden z-310 relative p-2">
+          <button
+            onClick={burgerVisible && !isClosing ? closeBurger : openBurger}
+            aria-label="Toggle Menu"
+          >
+            <div
+              className={`nav-toggle ${burgerVisible && !isClosing ? 'active' : ''}`}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </button>
         </div>
       </div>
@@ -90,19 +103,15 @@ export default function Nav() {
             onClick={closeBurger}
           />
           <div
-            className={`fixed inset-x-0 top-0 z-250 md:hidden flex justify-center ${isClosing ? 'animate-slide-up' : 'animate-slide-down'}`}
+            className={`fixed inset-x-0 top-0 z-250 md:hidden flex justify-center ${isClosing ? 'animate-menu-hide' : 'animate-menu-reveal'}`}
           >
             {/* Menu */}
             <div className="relative w-full bg-[linear-gradient(180deg,#B42525_0%,#E95252_35.19%,#FFA3A3_99.81%)] rounded-b-3xl py-3 pb-8 flex flex-col gap-4 z-100">
               {/* Header */}
-              <div className="flex justify-end px-2">
-                <button onClick={closeBurger} className="text-white">
-                  <IoIosClose size={50} />
-                </button>
-              </div>
+              <div className="h-10" />
 
               <div className="flex flex-col gap-4 text-lg font-medium items-center">
-                <NavLinks />
+                <NavLinks closeBurger={closeBurger} />
               </div>
               <div className="flex gap-10 pt-5 pb-2 justify-center">
                 <SocialLinks />
