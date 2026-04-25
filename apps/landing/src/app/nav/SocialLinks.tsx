571cd242bb7b6ede6071a759f23b63fa2a7c6009
alebora <148511093+alebora@users.noreply.github.com>
2026-04-25 00:36:05 -0400

    fixed typos removed fb icon

--- apps/landing/src/app/nav/SocialLinks.tsx
+++ apps/landing/src/app/nav/SocialLinks.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link';
 export default function SocialLinks() {
   return (
     <>
-      <Link
+      {/* <Link
         href="https://www.facebook.com/Hackthe6ix/"
         target="_blank"
         rel="sponsored"
         className="text-white hover:text-primary-200"
       >
         <FaFacebook size={22} />
-      </Link>
+      </Link> */}
       <Link
         href="https://www.instagram.com/hackthe6ix"
         target="_blank"
