792ec883c67d8fd097d41ab4087aadbc9297f430
alebora <148511093+alebora@users.noreply.github.com>
2026-04-25 13:43:43 -0400

    removed fb import

--- apps/landing/src/app/nav/SocialLinks.tsx
+++ apps/landing/src/app/nav/SocialLinks.tsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
 import Link from 'next/link';
 
