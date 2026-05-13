b6aa1995b2922548f7cf38d95bb3691d209bb3e7
alebora <148511093+alebora@users.noreply.github.com>
2026-05-12 22:28:03 -0400

    small fixes to components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -10,3 +10,7 @@ export * from './components/Button';
 export * from './components/Input';
 export * from './components/InputGroup';
 export * from './components/Typography';
+export * from './components/InputArea';
+export * from './components/Tooltip';
+export * from './components/UploadFile';
+export * from './components/Modal';
