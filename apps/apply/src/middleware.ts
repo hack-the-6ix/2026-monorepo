cb11d42c72edc98ebff4751d6d9d1247f7222d35
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-05-19 18:36:13 -0400

    comment out auth for now

--- apps/apply/src/middleware.ts
+++ apps/apply/src/middleware.ts
@@ -1,25 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-import { fetchWithCookies } from './actions';
+// import { fetchWithCookies } from './actions';
 
-export async function middleware(request: NextRequest) {
-  try {
-    const res = await fetchWithCookies(`${process.env.HT6_API_URL}/auth/check`);
-    if (!res.ok) throw new Error('Failed auth check. Triggering login');
-  } catch (err) {
-    console.error(err);
+// export async function middleware(request: NextRequest) {
+//   try {
+//     const res = await fetchWithCookies(`${process.env.HT6_API_URL}/auth/check`);
+//     if (!res.ok) throw new Error('Failed auth check. Triggering login');
+//   } catch (err) {
+//     console.error(err);
 
-    const loginUrl = new URL(`${process.env.HT6_API_URL}/auth/login`);
+//     const loginUrl = new URL(`${process.env.HT6_API_URL}/auth/login`);
 
-    const fallbackRedirectTarget =
-      process.env.HOST_URL || request.nextUrl.origin;
+//     const fallbackRedirectTarget =
+//       process.env.HOST_URL || request.nextUrl.origin;
 
-    loginUrl.searchParams.set('redirectUrl', fallbackRedirectTarget);
+//     loginUrl.searchParams.set('redirectUrl', fallbackRedirectTarget);
 
-    return NextResponse.redirect(loginUrl.toString());
-  }
-}
+//     return NextResponse.redirect(loginUrl.toString());
+//   }
+// }
 
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
+// export const config = {
+//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+// };
