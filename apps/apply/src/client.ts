6457da390ed2e00318b1f8a8146d0cceaf50b85a
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-05-21 04:10:02 -0400

    allow backend to set content-type

--- apps/apply/src/client.ts
+++ apps/apply/src/client.ts
@@ -28,7 +28,6 @@ export async function fetchHt6<T, P = unknown>(
     if (options.body instanceof FormData) {
       fetchOptions.body = options.body;
       delete headers['Content-Type'];
-      headers['Content-Type'] = 'multipart/form-data';
     } else {
       headers['Content-Type'] = 'application/json';
       fetchOptions.body = JSON.stringify(options.body);
