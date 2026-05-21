b118a9692fb5e7591f5bb9d7bf145d9a60d87a9f
fpunny <frederic.pun@gmail.com>
2026-05-21 00:39:52 -0400

    fixes to auth

--- apps/apply/src/middleware.ts
+++ apps/apply/src/middleware.ts
@@ -1,22 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { fetchWithCookies } from './actions';
+import { fetchWithCookies } from '@/actions';
 
 export async function middleware(request: NextRequest) {
   try {
-    const res = await fetchWithCookies(`${process.env.HT6_API_URL}/auth/check`);
+    const res = await fetchWithCookies(
+      request,
+      `${process.env.HT6_API_URL}/auth/check`,
+      {
+        headers: { Cookie: request.cookies.toString() },
+      },
+    );
     if (!res.ok) throw new Error('Failed auth check. Triggering login');
   } catch (err) {
     console.error(err);
-
     const loginUrl = new URL(`${process.env.HT6_API_URL}/auth/login`);
-
-    const fallbackRedirectTarget =
-      process.env.HOST_URL || request.nextUrl.origin;
-
-    loginUrl.searchParams.set('redirectUrl', fallbackRedirectTarget);
-
-    return NextResponse.redirect(loginUrl.toString());
+    loginUrl.searchParams.set('redirectUrl', process.env.HOST_URL!);
+    return NextResponse.redirect(loginUrl);
   }
 }
 
