8422520d0199b08764bc88d41d68645544c5a0c1
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-06-08 18:15:00 -0400

    add ticket image

--- apps/hacker/src/components/status/RsvpedView.tsx
+++ apps/hacker/src/components/status/RsvpedView.tsx
@@ -1,37 +1,42 @@
 import { Typography } from '@hackthe6ix/ui';
 
+import TicketImage from '../TicketImage';
+
 const RsvpedView = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[80vh] text-center px-4 space-y-4">
-      <Typography
-        as="p"
-        textSize="subtitle-sm"
-        textWeight="bold"
-        textColor="text-white"
-        className="mt-6 md:mt-32"
-      >
-        You’re all set!
-      </Typography>
-      <Typography
-        as="h1"
-        textSize="heading-lg"
-        textWeight="bold"
-        textColor="text-primary-400"
-      >
-        RSVP confirmed! 🎉
-      </Typography>
-      <Typography
-        as="p"
-        textSize="paragraph-lg"
-        textWeight="regular"
-        textColor="text-white"
-        className="max-w-xs md:max-w-none"
-      >
-        We&apos;ve received your information and can&apos;t wait to see what
-        you&apos;ll build at Hack the 6ix! <br />
-        Keep an eye on our emails for event updates, announcements, and next
-        steps.
-      </Typography>
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen pr-10 gap-17 py-10">
+      <div className="flex flex-col items-center justify-center space-y-4 text-center -translate-y-20 md:min-h-[80vh] md:w-[50%]">
+        <Typography
+          as="p"
+          textSize="subtitle-sm"
+          textWeight="bold"
+          textColor="text-white"
+          className="mt-6 md:mt-32"
+        >
+          You’re all set!
+        </Typography>
+        <Typography
+          as="h1"
+          textSize="heading-lg"
+          textWeight="bold"
+          textColor="text-primary-400"
+        >
+          RSVP confirmed! 🎉
+        </Typography>
+        <Typography
+          as="p"
+          textSize="paragraph-lg"
+          textWeight="regular"
+          textColor="text-white"
+          className="max-w-xs md:max-w-none"
+        >
+          We&apos;ve received your information and can&apos;t wait to see what
+          you&apos;ll build at Hack the 6ix! <br />
+          Keep an eye on our emails for event updates, announcements, and next
+          steps.
+        </Typography>
+      </div>
+      <TicketImage />
     </div>
   );
 };
