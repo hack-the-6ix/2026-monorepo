2ae9ec46524676177940f3a15a0f010afa832e25
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-06-07 23:53:30 -0400

    pls work

--- apps/hacker/src/types/images.d.ts
+++ apps/hacker/src/types/images.d.ts
@@ -1,1 +1,7 @@
 declare module '*.svg';
+declare module '@/assets/*.png';
+declare module '@/assets/*.jpg';
+declare module '@/assets/*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.svg';
