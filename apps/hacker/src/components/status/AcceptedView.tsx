16bd1a07cbce37871db25dd59f6878f3f8955d15
Lindsay Xie <70488690+Lindsay-X@users.noreply.github.com>
2026-06-07 21:42:23 -0400

    specific deadline

--- apps/hacker/src/components/status/AcceptedView.tsx
+++ apps/hacker/src/components/status/AcceptedView.tsx
@@ -40,7 +40,7 @@ const AcceptedView = ({ name, onDecline }: AcceptedViewProps) => {
         opportunity to hack with us.
         <br /> <br />
         To confirm your attendance, please RSVP below by{' '}
-        <span className="text-yellow-300">June 15th</span>.
+        <span className="text-yellow-300">June 14th 11:59PM EST</span>.
       </Typography>
 
       <div className="mt-4 w-full flex flex-col md:flex-row items-center justify-center gap-4">
