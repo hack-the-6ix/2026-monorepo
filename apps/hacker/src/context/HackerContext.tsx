9b758e4ccdddf7dec18e793f428d8d7927dce3ab
jason <jason_der@outlook.com>
2026-07-14 17:43:43 -0400

    lint

--- apps/hacker/src/context/HackerContext.tsx
+++ apps/hacker/src/context/HackerContext.tsx
@@ -87,6 +87,7 @@ export function HackerProvider({ children }: { children: ReactNode }) {
   };
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     void refresh();
   }, []);
 
